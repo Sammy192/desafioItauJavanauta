4d9916a23fa4854209d7a7d831772d8526ba4eee
Samuel Rodrigues Borges <samuel.borges@e-unicred.com.br>
2025-03-24 13:45:25 -0300

    Documenta api com swagger

--- src/main/java/com/javanauta/transacao_api/controllers/EstatisticasController.java
+++ src/main/java/com/javanauta/transacao_api/controllers/EstatisticasController.java
@@ -2,8 +2,12 @@ package com.javanauta.transacao_api.controllers;
 
 import com.javanauta.transacao_api.business.services.EstatisticasService;
 import com.javanauta.transacao_api.controllers.dtos.EstatisticasResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,14 @@ public class EstatisticasController {
 
     private final EstatisticasService estatisticasService;
 
+    @GetMapping
+    @Operation(description = "Buscar estatísticas de transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca efetuada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Parâmetros inválidos na transação"),
+            @ApiResponse(responseCode = "400", description = "Erro na busca de estatísticas de transações"  ),
+            @ApiResponse(responseCode = "500", description = "Erro interno no sistema")
+    })
     public ResponseEntity<EstatisticasResponseDTO> buscarEstatisticas(
             @RequestParam(value = "tempoInvervaloBusca", required = false, defaultValue = "60") Integer tempoInvervaloBusca) {
         EstatisticasResponseDTO estatisticasResponseDTO = estatisticasService.calculaEstatisticasTransacoes(tempoInvervaloBusca);
--- src/main/java/com/javanauta/transacao_api/controllers/TransacaoController.java
+++ src/main/java/com/javanauta/transacao_api/controllers/TransacaoController.java
@@ -2,6 +2,9 @@ package com.javanauta.transacao_api.controllers;
 
 import com.javanauta.transacao_api.business.services.TransacaoService;
 import com.javanauta.transacao_api.controllers.dtos.TransacaoRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +18,26 @@ public class TransacaoController {
     private final TransacaoService transacaoService;
 
     @PostMapping
+    @Operation(description = "Adicionar novas transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação adicionada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Parâmetros inválidos na transação"),
+            @ApiResponse(responseCode = "400", description = "Erro de validação na transação"  ),
+            @ApiResponse(responseCode = "500", description = "Erro interno no sistema")
+    })
     public ResponseEntity<Void> adicionarTransacao(@RequestBody TransacaoRequestDTO transacaoRequestDTO) {
         transacaoService.adicionarTransacoes(transacaoRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping
+    @Operation(description = "Deletar todas as transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transações deletadas com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Parâmetros inválidos na transação"),
+            @ApiResponse(responseCode = "400", description = "Erro de validação na transação"  ),
+            @ApiResponse(responseCode = "500", description = "Erro interno no sistema")
+    })
     public ResponseEntity<Void> limparTransacoes() {
         transacaoService.limpaTransacoes();
         return ResponseEntity.ok().build();
